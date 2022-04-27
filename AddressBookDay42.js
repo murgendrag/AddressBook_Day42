f45b9419d263fd2fc89031e13b74db65ca8b17fb
murgendrag <murgendra67@gmail.com>
2022-04-27 23:58:53 +0530

    Create contact

---
+++ AddressBookDay42.js
@@ -0,0 +1,24 @@
+class AddressBookDay42
+{
+    FirstName;
+    LastName;
+    Address;
+    City;
+    State;
+    Zip;
+    PhoneNumber;
+    Email;
+
+    constructor(...params)
+    {
+        this.FirstName=params[0];
+        this.LastName=params[1];
+        this.Address=params[2];
+        this.City=params[3];
+        this.State=params[4];
+        this.Zip=params[5];
+        this.PhoneNumber=params[6];
+        this.Email=params[7];
+    }
+    
+    
